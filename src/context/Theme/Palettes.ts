7201b8652d65d9212a32401caee638f73ea9c622
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-24 20:35:59 +0300

    Add background color to the Palette

--- src/context/Theme/Palettes.ts
+++ src/context/Theme/Palettes.ts
@@ -7,9 +7,10 @@ type Palette = {
   error: string;
   warning: string;
   paper: string;
-  textAction:string,
+  textAction: string;
   textPrimary: string;
   textSecondary: string;
+  background: string;
 };
 
 const primaryTheme: Palette = {
@@ -20,7 +21,8 @@ const primaryTheme: Palette = {
   warning: "#FFCC00",
   textPrimary: "#000000",
   textSecondary: "#414141",
-  textAction:'white',
+  textAction: "white",
+  background: "#f1f1f1",
 };
 const darkTheme: Palette = {
   primary: "#121212",
@@ -30,26 +32,27 @@ const darkTheme: Palette = {
   warning: "#FFCC00",
   textPrimary: "#fff",
   textSecondary: "#afafaf",
-  textAction:'white',
+  textAction: "white",
+  background: "#2f2f2f",
 };
 
-
 const greenTheme: Palette = {
-    primary: "#7FB77E",
-    secondary: "#B1D7B4",
-    error: "#B00020",
-    paper: "#F1F1F1",
-    warning: "#FFCC00",
-    textAction:'white',
-    textPrimary: "#000000",
-    textSecondary: "#414141",
-  };
-export type keys = "primary" | "green" | 'dark';
+  primary: "#7FB77E",
+  secondary: "#B1D7B4",
+  error: "#B00020",
+  paper: "#F1F1F1",
+  warning: "#FFCC00",
+  textAction: "white",
+  textPrimary: "#000000",
+  textSecondary: "#414141",
+  background: "#EBFDEE",
+};
+export type keys = "primary" | "green" | "dark";
 
 export const palettes: Record<keys, Palette> = {
   primary: primaryTheme,
   green: greenTheme,
-  dark:darkTheme,
+  dark: darkTheme,
 };
 
 export default Palette;
