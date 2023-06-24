7374d9834f1b989adc592905b6a965be4b9e4085
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-24 17:24:57 +0300

    Add 2 test cases for sorting handler:

--- src/Components/CartTable/index.test.tsx
+++ src/Components/CartTable/index.test.tsx
@@ -17,5 +17,28 @@ describe("CartTable", () => {
     const filteredItems = getAllByTestId("cart-item");
     expect(filteredItems.length).toBeLessThan(items.length);
   });
+  test("sorts items in ascending order when clicking on column header", () => {
+    const { getAllByTestId, getByText } = render(<CartTable />);
+    const taxColumnHeader = getByText("TAX");
 
+    fireEvent.click(taxColumnHeader);
+    fireEvent.click(taxColumnHeader);
+
+    const items = getAllByTestId("cart-item");
+    const firstItemTax = parseInt(items[0].childNodes[1].textContent!);
+    const secondItemTax = parseInt(items[1].childNodes[1].textContent!);
+    expect(firstItemTax).toBeLessThanOrEqual(secondItemTax);
+  });
+
+  test("resets the sort order when clicking on the same column header", () => {
+    const { getAllByTestId, getByText } = render(<CartTable />);
+    const taxColumnHeader = getByText("TAX");
+
+    fireEvent.click(taxColumnHeader);
+
+    const items = getAllByTestId("cart-item");
+    const firstItemTax = parseInt(items[0].childNodes[1].textContent!);
+    const secondItemTax = parseInt(items[1].childNodes[1].textContent!);
+    expect(firstItemTax).toBeGreaterThanOrEqual(secondItemTax);
+  });
 });
