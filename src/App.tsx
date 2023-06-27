a3db966eed15c34488e6aa6dea0043bb5082a258
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-27 11:56:04 +0300

    Remove ubused imported files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
 import Router from "./Router";
 import ThemeProvider from "./context/Theme/ThemeProvider";
 
