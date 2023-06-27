1083c4fa88db06060cc936c60c06873fd64aac4f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-27 11:53:41 +0300

    Creating Responsive drawer

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -3,11 +3,18 @@ import CartTable from "../CartTable";
 import style from "./style.module.css";
 import useTheme from "../../context/Theme/useTheme";
 import SingleCart from "../SingleCart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeftLong,
+  faArrowLeftRotate,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
 interface props {
   width?: string;
 }
 const Aside: FC<props> = ({ width }) => {
   const [show, setShow] = useState<string>("");
+  const [showContainer, setShowContainer] = useState<boolean>(true)
   const theme = useTheme();
   const chooseOrderHandler = (id: string) => {
     setShow(id);
@@ -19,8 +26,22 @@ const Aside: FC<props> = ({ width }) => {
         backgroundColor: theme.palette.paper,
         width: width,
         boxShadow: " 0 2px 4px" + theme.palette.shadow,
+        right:showContainer ? 0: '-100%' 
       }}
+      onBlur={()=>setShowContainer(false)}
     >
+      <FontAwesomeIcon
+        className={style.arrowLeft}
+        icon={faArrowLeftLong}
+        color={theme.palette.textPrimary}
+        onClick={()=>setShowContainer(true)}
+      />
+        <FontAwesomeIcon
+        className={style.arrowRight}
+        icon={faArrowRightLong}
+        color={theme.palette.textPrimary}
+        onClick={()=>setShowContainer(false)}
+      />
       {show === "" ? (
         <CartTable onChoose={chooseOrderHandler} />
       ) : (
