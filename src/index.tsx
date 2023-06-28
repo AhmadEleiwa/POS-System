f0c07d5b976d51458ecd2601a9997ef76d1762bf
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-28 21:15:48 +0300

    Configure the Store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./store/Reducers";
+import { Provider } from "react-redux";
 
-
+const store = configureStore({ reducer: rootReducer});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
-
