710d18d9d659d5c7b87946d3a8a8dc2c82936a13
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-29 20:28:11 +0300

    Chnage the name of actions for each reducer

--- src/store/Reducers/index.ts
+++ src/store/Reducers/index.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import productsReducer from './products'
 import categoriessReducer from './categories'
+import unitOfMeasureReducer from './unitOfMeasure'
 
 
 const rootReducer = combineReducers({
     productsReducer:productsReducer,
     categoriessReducer:categoriessReducer,
+    unitOfMeasureReducer:unitOfMeasureReducer
 
 })
 
