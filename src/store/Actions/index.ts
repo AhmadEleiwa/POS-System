5214425761bf963e800f7aae0e4bb0a10630418a
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-29 20:29:09 +0300

    Add unit of measure actions + change name of category and products actions

--- src/store/Actions/index.ts
+++ src/store/Actions/index.ts
@@ -1,30 +1,51 @@
 export const addProduct = () =>{
     return {
-        type:'ADD'
+        type:'ADD_PRODUCT'
     }
 }
 export const removeProduct = (id:string) =>{
     return {
-        type:'REMOVE',
+        type:'REMOVE_PRODUCT',
         data:id
     }
 }
 export const addCategory = (name:string )=>{
     return {
-        type:'ADD',
+        type:'ADD_CATEGORY',
         data:name
     }
 }
 
 export const updateCategory = (name:string, newName:string )=>{
     return {
-        type:'UPDATE',
+        type:'UPDATE_CATEGORY',
         data:{name:name,newName:newName         }
     }
 }
 export const removeCategory = (name:string )=>{
     return {
-        type:'REMOVE',
+        type:'REMOVE_CATEGORY',
+        data:name
+    }
+}
+
+
+export const addUnit = (unit:UnitOfMeasure )=>{
+    return {
+        type:'ADD_UNIT',
+        data:unit
+    }
+}
+
+export const updateUnit = (name:string, newUnit:UnitOfMeasure )=>{
+    return {
+        type:'UPDATE_UNIT',
+        data:{name:name,newUnit:newUnit         }
+    }
+}
+export const removeUnit = (name:string )=>{
+    return {
+        type:'REMOVE_UNIT',
         data:name
     }
 }
