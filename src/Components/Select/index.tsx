7733bf9b4086f6ec36a7904b2afa6b8ab6e3ad01
ahmadeleiwa <ahmadilawa@gmail.com>
2023-06-29 13:49:28 +0300

    OnChange handler tpye into ChangeEventHandle instead from string function args

--- src/Components/Select/index.tsx
+++ src/Components/Select/index.tsx
@@ -1,32 +1,29 @@
-import React, { ChangeEvent, FC } from "react";
+import React, { ChangeEvent, ChangeEventHandler, FC } from "react";
 import style from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import useTheme from "../../context/Theme/useTheme";
-import Button from "../Button";
 interface props {
   options: { key: string; value: string }[];
-  onChange?: (value: string) => void;
+  onChange?: ChangeEventHandler<HTMLSelectElement>;
+  width?:string;
 }
-const Select: FC<props> = ({ options, onChange }) => {
+const Select: FC<props> = ({ options, onChange,width }) => {
   const theme = useTheme();
-  const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
-    if (onChange) {
-      onChange(event.target.value);
-    }
-  };
+
   return (
     <div
       className={style.select}
       style={{
         border: "1px solid" + theme.palette.secondary,
         backgroundColor: theme.palette.paper,
+        width:width
       }}
     >
       <select
         name=""
         id=""
-        onChange={onChangeHandler}
+        onChange={onChange}
         className={style.select}
         style={{ color: theme.palette.textPrimary }}
       >
