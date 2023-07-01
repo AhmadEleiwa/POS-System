418abe67615a0ca402c72e756ca72c9e4cb141bf
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-01 17:34:51 +0300

    Get the initial data from the static json file

--- src/store/Reducers/products.ts
+++ src/store/Reducers/products.ts
@@ -1,41 +1,4 @@
-let initialData = [
-  {
-    title: "burger",
-    price: 2.33,
-    id: "1",
-    media: "https://picsum.photos/200/300",
-    unitOfMeasure: {
-      unitOfMeasureName: "0.5kg",
-      baseUnitOfMeasure: "g",
-      conversionFactor: 500,
-    },
-    category: { categoryName: "Meat" },
-  },
-  {
-    title: "Coco cola",
-    price: 1,
-    id: "2",
-    media: "https://picsum.photos/200/301",
-    unitOfMeasure: {
-      unitOfMeasureName: "330ml",
-      baseUnitOfMeasure: "l",
-      conversionFactor: 0.33,
-    },
-    category: { categoryName: "Drink" },
-  },
-  {
-    title: "Coco cola",
-    price: 1,
-    id: "3",
-    media: "https://picsum.photos/200/302",
-    unitOfMeasure: {
-      unitOfMeasureName: "1.5l",
-      baseUnitOfMeasure: "l",
-      conversionFactor: 1.5,
-    },
-    category: { categoryName: "Drink" },
-  },
-];
+import initialData from '../../Static/Products.json'
 const productsReducer = (state: Product[] = initialData, action: Action) => {
   switch (action.type) {
     case "ADD_PRODUCT":
