929786558efe5a2c777f4e8e9992979f376c71fa
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-06 16:55:43 +0300

    Hanlde the product add handler

--- src/Components/ProductRow/index.tsx
+++ src/Components/ProductRow/index.tsx
@@ -1,6 +1,10 @@
 import React, { FC, MouseEventHandler } from "react";
 import style from "./style.module.css";
 import useTheme from "../../context/Theme/useTheme";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { useSelector } from "react-redux";
+import { addProductToCart } from "../../store/Actions";
+import { RootState } from "../../store/Reducers";
 interface props {
   title: string;
   unitOfMeasure: string;
@@ -9,6 +13,7 @@ interface props {
   className?: string;
   onClick?: MouseEventHandler;
   width?: string;
+  id?:string;
   borderRadius?: string;
 }
 const ProductRow: FC<props> = ({
@@ -19,9 +24,22 @@ const ProductRow: FC<props> = ({
   className,
   width,
   onClick,
+  id,
   borderRadius = "4px",
 }) => {
   const theme = useTheme();
+  const dispatch = useDispatch();
+  const cartId = useSelector<RootState>(
+    (state) => state.selectedCartReducer
+  ) as string;
+  const products = useSelector<RootState>(
+    (state) => state.productsReducer
+  ) as Product[];
+  const addHandler = () => {
+    // some logic to handle the adding to order
+    let isFound = products.find((p) => p.id === id);
+    if (isFound) dispatch(addProductToCart(cartId, isFound));
+  };
   return (
     <div
       className={style.row + " " + className}
@@ -30,7 +48,7 @@ const ProductRow: FC<props> = ({
         width: width,
         borderRadius: borderRadius,
       }}
-      onClick={onClick}
+      onClick={onClick ? onClick : addHandler}
     >
       <img src={media} alt="" />
       <p style={{ color: theme.palette.textPrimary }}>{title}</p>
