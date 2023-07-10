7e394caad684c19ea01c23e78f3e36681ffaaa3c
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-11 01:18:48 +0300

    Set catgroies action

--- src/store/Reducers/categories.ts
+++ src/store/Reducers/categories.ts
@@ -1,20 +1,21 @@
-import Categoris from '../../Static/Categories.json'
-
-  const categoriessReducer = (state: Category[] = Categoris, action: Action) => {
-    switch (action.type) {
-      case "ADD_CATEGORY":
-        return [...state, {categoryName: action.data}];
-      case "UPDATE_CATEGORY":{
-        let dd =state.find(p => p.categoryName === action.data.name) 
-        dd!.categoryName = action.data.newName
-        return [...state]
-      }
-      case "REMOVE_CATEGORY":return state.filter(p => p.categoryName!== action.data)
-      default:
-        return state;
+const categoriessReducer = (state: Category[] = [], action: Action) => {
+  switch (action.type) {
+    case "SET_CATEGORIES":
+      return action.data;
+    case "ADD_CATEGORY":
+      return [...state, { categoryName: action.data }];
+    case "UPDATE_CATEGORY": {
+      return state.map((p) => {
+        if (p.categoryName === action.data.name)
+          return { categoryName: action.data.newName };
+        return p;
+      });
     }
-  };
+    case "REMOVE_CATEGORY":
+      return state.filter((p) => p.categoryName !== action.data);
+    default:
+      return state;
+  }
+};
 
-  
-  export default categoriessReducer;
-  
+export default categoriessReducer;
