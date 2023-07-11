89ef71c243058fe580d8e2e6df69e1393bc71017
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-11 11:39:16 +0300

    Working on dashborad and setting to theme colors

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, Key } from "react";
+import Select from "../../Components/Select";
+import useTheme from "../../context/Theme/useTheme";
+import { keys } from "../../context/Theme/Palettes";
 
 /**
- * ## Dashboard page 
+ * ## Dashboard page
  * Dashboard page the page that allow the user or admins to manipulate users settings
  * And the system sittings
  */
-const Dashboard: FC= () => {
-  return <div></div>;
+const Dashboard: FC = () => {
+  const theme = useTheme();
+  return (
+    <div>
+      <div>
+        <Select
+          onChange={(value) => theme.changePalette(value.target.value as keys)}
+          options={[
+            { key: "dark", value: "dark" },
+            { key: "primary", value: "white" },
+            { key: "green", value: "green" },
+            { key: "matrial", value: "matrial" },
+
+          ]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
