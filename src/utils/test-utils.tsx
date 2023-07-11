33e5709793315f0ea18b1f6699a8670ad27ac036
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-11 20:38:32 +0300

    Create Wrapper component to pass carts into CartTable

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { RenderOptions } from "@testing-library/react";
 import { FC, PropsWithChildren } from "react";
-import { render } from '@testing-library/react'
-import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import { Provider, useSelector } from "react-redux";
 import { PreloadedState } from "redux";
 import rootReducer, { RootState } from "../store/Reducers";
 import { ToolkitStore } from "@reduxjs/toolkit/dist/configureStore";
+import CartTable from "../Components/CartTable";
+import { Form, Formik } from "formik";
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
   preloadedState?: PreloadedState<RootState>;
@@ -15,8 +17,8 @@ export function renderWithProviders(
   ui: React.ReactElement,
   {
     preloadedState = {},
-    // Automatically create a store instance if no store was passed in
-    store = configureStore({reducer:rootReducer}),
+
+    store = configureStore({ reducer: rootReducer }),
     ...renderOptions
   }: ExtendedRenderOptions = {}
 ) {
@@ -28,6 +30,19 @@ export function renderWithProviders(
     );
   };
 
-  // Return an object with the store and all of RTL's query functions
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
+interface TestProps extends PropsWithChildren{
+  init:any;
+}
+export const FormTest: FC<TestProps> = ({ children, init }) => {
+  return (
+    <Formik initialValues={init} onSubmit={() => {}}>
+      <Form>{children}</Form>
+    </Formik>
+  );
+};
+export const CartTableWithCartState: FC = () => {
+  const carts = useSelector<RootState>((state) => state.cartsReducer) as Cart[];
+  return <CartTable carts={carts} onChoose={() => {}} />;
+};
