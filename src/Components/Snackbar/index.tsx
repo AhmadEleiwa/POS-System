9a3db664669494c4d685a3acc0950bbefdf9bccd
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-12 18:27:49 +0300

    Create Snackbar

---
+++ src/Components/Snackbar/index.tsx
@@ -0,0 +1,67 @@
+import { FC, useEffect, useRef } from "react";
+import style from "./style.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import useTheme from "../../context/Theme/useTheme";
+interface props {
+  response: ResponseResult;
+  onClose: () => void;
+}
+let id = 0;
+const Snackbar: FC<props> = ({ response, onClose }) => {
+  const theme = useTheme();
+  const snackbar = useRef<HTMLDivElement>(null);
+  let color = theme.palette.secondary;
+  if (response.status >= 300 && response.status < 400) {
+    color = theme.palette.warning;
+  } else if (response.status >= 400) {
+    color = theme.palette.error;
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      if (snackbar.current) snackbar.current.style.opacity = "0.9";
+    }, 0);
+    setTimeout(
+      (i: number) => {
+        // The id to identify the last snackbar are running (Last massage)
+        if (i + 1 === id) {
+          id = 0;
+          onClose();
+        }
+      },
+      5000,
+      id++
+    );
+  }, [response, onClose]);
+
+  return (
+    <>
+      {response.message && (
+        <div
+          ref={snackbar}
+          className={style.snackbar}
+          style={{
+            backgroundColor: theme.palette.paper,
+            boxShadow: "0 2px 8px" + theme.palette.shadow,
+            opacity: "0",
+          }}
+        >
+          <FontAwesomeIcon
+            className={style.xIcon}
+            icon={faX}
+            cursor={"pointer"}
+            onClick={onClose}
+          />
+          <div className={style.message}>{response.message}</div>
+          <div
+            className={style.status}
+            style={{ backgroundColor: color, color: theme.palette.textAction }}
+          >
+            {response.status}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default Snackbar;
