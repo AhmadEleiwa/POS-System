38720b31a8590e82eaea1c52529738458b90ce8f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-07-12 20:05:33 +0300

    Adding the theme provider and the snackBar provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,19 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./store/Reducers";
 import { Provider } from "react-redux";
+import SnackProvider from "./context/Snackbar/SnackProvider";
+import ThemeProvider from "./context/Theme/ThemeProvider";
 
-const store = configureStore({ reducer: rootReducer});
+const store = configureStore({ reducer: rootReducer });
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+      <SnackProvider>
+        <App />
+      </SnackProvider>
+    </ThemeProvider>
   </Provider>
 );
